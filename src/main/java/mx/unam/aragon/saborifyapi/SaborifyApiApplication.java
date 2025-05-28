07c6dde96efe0cf00996a4e80abe908742e93dc3
hugo_pzz <hugo.ortz.04@gmail.com>
2025-05-27 22:55:40 -0600

    Initial commit

---
+++ src/main/java/mx/unam/aragon/saborifyapi/SaborifyApiApplication.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.saborifyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaborifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaborifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/unam/aragon/saborifyapi/SaborifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon.saborifyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaborifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
